c04a1fe7c7786df3e4c2b9dc1bb3bbdb6602d87f
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-06 01:11:18 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,7 +35,7 @@ def before_request():
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
-        '/api/v1/auth_session/login'
+        '/api/v1/auth_session/login/'
     ]
     if auth is None:
         return
