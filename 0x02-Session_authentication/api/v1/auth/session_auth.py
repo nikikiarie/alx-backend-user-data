7f6cdc853ecf62b16f5b4fe148aa2ce9d7d92bea
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-05 23:40:01 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,10 +4,19 @@ SessionAuth class
 """
 from flask import abort, request
 from api.v1.auth.auth import Auth
-import uuid
+from uuid import uuid4
 from models.user import User
 
 
 class SessionAuth(Auth):
     """ SessionAuth class """
-    pass
+    user_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if not isinstance(user_id, str) or not user_id:
+            return
+        session_id = str(uuid4())
+
+        self.user_session_id[session_id] = user_id
+
+        return session_id
