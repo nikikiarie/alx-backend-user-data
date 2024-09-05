b6a8974306be5dc39c1cddbc9fa0d4a97e9206f5
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-05 23:41:41 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,13 +10,13 @@ from models.user import User
 
 class SessionAuth(Auth):
     """ SessionAuth class """
-    user_session_id = {}
+    user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         if not isinstance(user_id, str) or not user_id:
             return
         session_id = str(uuid4())
 
-        self.user_session_id[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
 
         return session_id
