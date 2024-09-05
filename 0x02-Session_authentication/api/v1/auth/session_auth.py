e37ba2d8a6e2eca54e81a5b6e3257d0a3d896fae
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-06 00:07:32 +0300

    User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,3 +21,10 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if not session_id or not isinstance(session_id, str):
+            return
+        user_id = self.user_id_by_session_id.get(session_id)
+
+        return user_id
