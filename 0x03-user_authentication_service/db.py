a9b9e0e30bea2af81b19580b0fbc20f5a0b6c82e
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-11 19:41:52 +0300

    user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,7 +45,7 @@ class DB:
             if not hasattr(User, i):
                 raise InvalidRequestError()
 
-        user = self.session.query(User).filter_by(**kwargs).first()
+        user = self._session.query(User).filter_by(**kwargs).first()
 
         if user:
             return user
