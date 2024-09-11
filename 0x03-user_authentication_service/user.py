225fd32650e7e35d02204bf56e6b349fffd26653
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-11 19:35:53 +0300

    user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -16,6 +16,6 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(20), nullable=False)
-    hash_password = Column(String(20), nullable=False)
+    hashed_password = Column(String(20), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
