60c5ff20b74ec2b2913576443a713db47bba3223
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-11 18:21:35 +0300

    USerModel

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+User module
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    User table
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(20), nullable=False)
+    hash_password = Column(String(20), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
+
+
+if __name__ == "__main__":
+    print(User.__tablename__)
+
+    for column in User.__table__.columns:
+        print("{}: {}".format(column, column.type))
