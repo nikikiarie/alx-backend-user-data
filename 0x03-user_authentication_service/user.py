7186451aad2a2f5a97c9b9800273eb5b50479848
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-11 22:49:36 +0300

    Find user by session ID

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,7 +14,7 @@ class User(Base):
     User table
     """
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(20), nullable=False)
     hashed_password = Column(String(20), nullable=False)
     session_id = Column(String(250))
