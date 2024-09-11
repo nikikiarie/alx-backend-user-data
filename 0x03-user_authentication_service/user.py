070c4cd283b0e3fd408bb2f193b7848a564e902e
nikikiarie <nix.kiariebiz@gmail.com>
2024-09-11 19:07:12 +0300

    exe

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 User module
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -18,4 +18,3 @@ class User(Base):
     hash_password = Column(String(20), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
-
